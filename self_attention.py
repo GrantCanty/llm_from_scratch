083db43fcae54001d54205db3576c7a6cc539a8a
Grant Canty <gc933815@ohio.edu>
2025-10-25 17:02:23 +0200

    added causal attention head

--- self_attention.py
+++ self_attention.py
@@ -37,3 +37,37 @@ class SelfAttentionV2(torch.nn.Module):
         context_vec = attn_weights @ values
 
         return context_vec
+    
+
+class CausalSelfAttention(torch.nn.Module):
+    def __init__(self, d_in, d_out, context_len, dropout):
+        super().__init__()
+        torch.manual_seed(123)
+        self.d_out = d_out
+        self.W_query = torch.nn.Linear(d_in, d_out, bias=False)
+        self.W_key = torch.nn.Linear(d_in, d_out, bias=False)
+        self.W_value = torch.nn.Linear(d_in, d_out, bias=False)
+        self.context_len = context_len
+        self.dropout = torch.nn.Dropout(dropout)
+        self.register_buffer(
+            'mask',
+            torch.tril(torch.ones(self.context_len, self.context_len), diagonal=0)
+        )
+
+    def forward(self, x):
+        
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2)
+        masked = attn_scores.masked_fill_(~self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(masked / keys.shape[-1]**.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        print(attn_weights)
+        context_vec = attn_weights @ values
+
+        print(context_vec)
+        return context_vec
