5f5d526282aaaea191782ef3b51a9b3b44389e21
Grant Canty <gc933815@ohio.edu>
2025-11-19 21:31:37 +0100

    removed excess text from decoder module

--- decoder.py
+++ decoder.py
@@ -6,18 +6,6 @@ from gpt_download import download_and_load_gpt2
 import embeddings
 
 
-'''tokenizer = tiktoken.get_encoding('gpt2')
-torch.manual_seed(123)
-settings, params = download_and_load_gpt2(model_size='124M', models_dir='gpt2')
-
-gpt_model = model.GPTModel(config.GPT_CONFIG_124M_INFER)
-model.load_weights(gpt_model, params)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-gpt_model.to(device)
-model.generate_and_print_sample(gpt_model, tokenizer, device, "Every step moves you", max_new_tokens=50, temperature=1.4, top_k=35)'''
-
-#settings, params = download_and_load_gpt2(model_size='124M', models_dir='gpt2')
-
 class Decoder():
     def __init__(self, param_count: int = 0, layer_count: int = 0, transformer_count: int = 0, tokenizer = tiktoken.get_encoding('gpt2')):
         torch.manual_seed(123)
